6d69a5c62bf14177cc09dc58480db9d50f180d4f
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-10-28 19:27:20 -0400

    got postgres setup

--- controllers/postgresController.js
+++ controllers/postgresController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const db = require("../db/queries");
 
 // get all posts
 
 exports.all_usernames_get = asyncHandler(async (req, res, next) => {
-
-
-   res.send('respond with a resource');
+const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(", "));
 
 });
